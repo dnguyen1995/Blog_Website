6c0dbb354da514fa46459d557d3f2b5d2cb7b3d8
Duc Nguyen <ducnguyen95@hotmail.com>
2021-09-21 21:43:52 +1000

    progress on blog

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -12,21 +13,41 @@ const contactText="Contact Us";
 const aboutText="About Us";
 const composeText="Compose";
 
+const blogList =[];
+const fullText=[];
+var blogCount=blogList.length;
+
+app.get("/",function(request,response){
+  response.render("blog",{bodyTitle:homeText,blogs:blogList});
+});
+
+app.post("/",function(request,response){
+  const blogTitle = request.body.blogTitle;
+  const blogText = request.body.blogText;
+
+  function trimText(text){
+    if(text.length>100){
+      return text.substring(0,101) + "...";
+    }else{
+      return text;
+    }
+  }
+  function createBlog(title,blog){
+    this.title=title,
+    this.subBlog=trimText(blog)
+    this.fullText=blog;
+  }
+  const Blog = new createBlog(blogTitle,blogText);
+  blogList.push(Blog);
+  console.log(blogList);
+
+  response.redirect("/");
+});
+
 app.get("/compose",function(request,response){
   response.render("compose",{bodyTitle:composeText});
 });
 
-app.get("/about",function(request,response){
-  response.render("about",{bodyTitle:aboutText});
-});
-
-app.get("/contact",function(request,response){
-  response.render("contact",{bodyTitle:contactText});
-});
-
-app.get("/",function(request,response){
-  response.render("blog",{bodyTitle:homeText});
-});
 
 app.listen(3000,function(){
   console.log("Blog server online at port 3000");
---
+++ views/index.js
@@ -0,0 +1,8 @@
+for(var i=0;i<document.querySelectorAll(".links").length;i++){
+  document.querySelector(".links")[i].style.color="yellow";
+  // document.querySelector(".links")[i].addEventListener("click",function(){
+  //   this.style.color="green";
+  // });
+}
+
+document.querySelector(".blogTitle")[i].style.color="yellow";
